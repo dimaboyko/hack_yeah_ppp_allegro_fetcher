37faa03a433acd6d0377ef68ac2cbd49b8edface
Radek Rochmalski <radek@wredny.net>
2017-10-29 09:08:10 +0100

    fetch data in background

--- app/services/external/allegro/fetcher.rb
+++ app/services/external/allegro/fetcher.rb
@@ -12,31 +12,9 @@ module External
 
         auctions.in_groups_of(BATCH_SIZE, false) do |group|
           group.each do |auction|
-            auction_data = ::Scarpers::Auction.perform(auction['itemId'])
-            auctioneer_data = ::Scarpers::Auctioneer.perform(auction_data[:auctioneer_id])
-            krs_data = ::Scarpers::Krs.perform(auctioneer_data['company_nip'])
-
-            hack_yeah_app_client.create_auction(
-              {
-                auctioneer_id: auction_data[:auctioneer_id],
-                auction_id: auction['itemId'],
-                auctioneer_data: auctioneer_data.merge(krs_data),
-                auction_data: auction_data[:auction_data],
-                auction_provider: PROVIDER
-              }
-            )
+            AdapterWorker.perform_async(auction)
           end
         end
-      end
-
-      private
-
-      def allegro_client
-        @allegro_client ||= ::External::Allegro::Client.instance
-      end
-
-      def hack_yeah_app_client
-        @hack_yeah_app_client ||= ::External::HackYeahApp::Client.instance
       end
     end
   end
---
+++ app/workers/adapter_worker.rb
@@ -0,0 +1,29 @@
+class AdapterWorker
+  include Sidekiq::Worker
+
+  def perform(auction)
+    auction_data = ::Scarpers::Auction.perform(auction['itemId'])
+    auctioneer_data = ::Scarpers::Auctioneer.perform(auction_data[:auctioneer_id])
+    krs_data = ::Scarpers::Krs.perform(auctioneer_data['company_nip'])
+
+    hack_yeah_app_client.create_auction(
+      {
+        auctioneer_id: auction_data[:auctioneer_id],
+        auction_id: auction['itemId'],
+        auctioneer_data: auctioneer_data.merge(krs_data),
+        auction_data: auction_data[:auction_data],
+        auction_provider: PROVIDER
+      }
+    )
+  end
+
+  private
+
+  def allegro_client
+    @allegro_client ||= ::External::Allegro::Client.instance
+  end
+
+  def hack_yeah_app_client
+    @hack_yeah_app_client ||= ::External::HackYeahApp::Client.instance
+  end
+end
